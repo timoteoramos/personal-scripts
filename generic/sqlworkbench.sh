41a5c130a163799d91718a66a5f714b354a5e218
Timoteo Ramos <timoteoramos@ymail.com>
2020-07-29 00:43:55 -0300

    SQL Workbench

---
+++ generic/sqlworkbench.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+SETUP_DIR=/opt/sqlworkbench
+TEMP_FILE=/tmp/sqlworkbench.zip
+
+curl -o $TEMP_FILE http://www.sql-workbench.eu/Workbench-Build125-with-optional-libs.zip
+if [ -d "$SETUP_DIR" ]; then rm -Rf $SETUP_DIR; fi
+mkdir -p $SETUP_DIR
+mkdir -p /usr/local/bin
+mkdir -p /usr/local/share/applications
+unzip -d $SETUP_DIR/ $TEMP_FILE
+chmod +x /opt/sqlworkbench/sql*.sh
+ln -s /opt/sqlworkbench/sqlwbconsole.sh /usr/local/bin/
+ln -s /opt/sqlworkbench/sqlworkbench.sh /usr/local/bin/
+
+cat << EOF > /usr/local/share/applications/sqlworkbench.desktop
+[Desktop Entry]
+Version=1.0
+Name=SQL Workbench/J
+Comment=Free, DBMS-independent, cross-platform SQL query tool
+Categories=Development;Utility;Database;
+Exec=/opt/sqlworkbench/sqlworkbench.sh
+Icon=/opt/sqlworkbench/workbench32.png
+Terminal=false
+Type=Application
+EOF
+
+rm $TEMP_FILE
