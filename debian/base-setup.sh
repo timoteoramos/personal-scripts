b5ade6826083e573100db8b29594f49b74699ffd
Timoteo Ramos <timoteoramos@ymail.com>
2020-07-29 00:44:47 -0300

    Debian basic setup

---
+++ debian/base-setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+apt update
+apt install -y build-essential git zsh curl wget tmux htop vim ranger sysstat
+
+cat << EOF > /etc/tmux.conf
+set -g default-command "\${SHELL}"
+set -g default-terminal "screen-256color"
+EOF
+
+cat << EOF > /etc/zsh/zshenv
+# /etc/zsh/zshenv: system-wide .zshenv file for zsh(1).
+#
+# This file is sourced on all invocations of the shell.
+# If the -f flag is present or if the NO_RCS option is
+# set within this file, all other initialization files
+# are skipped.
+#
+# This file should contain commands to set the command
+# search path, plus other important environment variables.
+# This file should not contain commands that produce
+# output or assume the shell is attached to a tty.
+#
+# Global Order: zshenv, zprofile, zshrc, zlogin
+
+if [[ -z "\$PATH" || "\$PATH" == "/bin:/usr/bin" ]]
+then
+	export PATH="/usr/local/bin:/usr/bin:/bin:/usr/games"
+fi
+
+EDITOR=vim.basic
+HISTFILE=~/.zsh_history
+HISTSIZE=1024
+SAVEHIST=1024
+VISUAL=vim.basic
+EOF
