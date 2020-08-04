3b619c321564976d0a0735940e0a0e80678cd5cc
Timoteo Ramos <timoteoramos@ymail.com>
2020-08-04 16:06:44 -0300

    Allow distro or codename override on Docker Debian

--- debian/docker.sh
+++ debian/docker.sh
@@ -4,8 +4,8 @@ apt remove --purge -y docker docker-engine docker.io containerd runc
 apt update
 apt install -y apt-transport-https ca-certificates curl gnupg-agent lsb-release software-properties-common
 
-DISTRO=`lsb_release -is | awk '{print tolower($0)}'`
-CODENAME=`lsb_release -cs`
+if [ -z $DISTRO ]; then DISTRO=`lsb_release -is | awk '{print tolower($0)}'`; fi
+if [ -z $CODENAME ]; then CODENAME=`lsb_release -cs`; fi
 
 curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | apt-key add -
 add-apt-repository "deb https://download.docker.com/linux/$DISTRO $CODENAME stable"
