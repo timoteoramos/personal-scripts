76befa7d26753c3e0069ef1164a15ea7bf7cabb8
Timóteo Ramos <timoteoramos@ymail.com>
2020-09-27 10:24:19 -0300

    Limit Docker repository to amd64

--- debian/docker.sh
+++ debian/docker.sh
@@ -8,6 +8,6 @@ if [ -z $DISTRO ]; then DISTRO=`lsb_release -is | awk '{print tolower($0)}'`; fi
 if [ -z $CODENAME ]; then CODENAME=`lsb_release -cs`; fi
 
 curl -fsSL https://download.docker.com/linux/$DISTRO/gpg | apt-key add -
-add-apt-repository "deb https://download.docker.com/linux/$DISTRO $CODENAME stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME stable"
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-compose
