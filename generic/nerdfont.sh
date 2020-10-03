8f54111720f531c8a9dfb051f0d332e5b2446e1b
Timóteo Ramos <timoteoramos@ymail.com>
2020-10-02 22:24:02 -0300

    Replace UbuntuMono with FiraMono

--- generic/nerdfont.sh
+++ generic/nerdfont.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget -O /tmp/UbuntuMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
-mkdir -p /usr/local/share/fonts/UbuntuMono
-unzip -d /usr/local/share/fonts/UbuntuMono /tmp/UbuntuMono.zip
-rm /usr/local/share/fonts/UbuntuMono/*Windows*
+wget -O /tmp/FiraMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
+mkdir -p /usr/local/share/fonts/FiraMono
+unzip -o -d /usr/local/share/fonts/FiraMono /tmp/FiraMono.zip
+rm /usr/local/share/fonts/FiraMono/*Windows*
+rm /usr/local/share/fonts/FiraMono/Fura*
 fc-cache -fv /usr/local/share/fonts
