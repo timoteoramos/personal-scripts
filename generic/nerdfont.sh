7f191b99da7ab1b0d623a2c1290b5fca61af1941
Timóteo Ramos <timoteoramos@ymail.com>
2020-10-27 01:58:54 -0300

    Interactive Nerd Font setup

--- generic/nerdfont.sh
+++ generic/nerdfont.sh
@@ -1,8 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-wget -O /tmp/FiraMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip
-mkdir -p /usr/local/share/fonts/FiraMono
-unzip -o -d /usr/local/share/fonts/FiraMono /tmp/FiraMono.zip
-rm /usr/local/share/fonts/FiraMono/*Windows*
-rm /usr/local/share/fonts/FiraMono/Fura*
-fc-cache -fv /usr/local/share/fonts
+if [ -z $PREFIX ]; then PREFIX=/usr/local; fi
+if [ -z $TMP ]; then TMP=/tmp; fi
+
+install_nerdfont () {
+    wget -O $TMP/$1.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$1.zip
+    mkdir -p $PREFIX/share/fonts/$1
+    unzip -o -d $PREFIX/share/fonts/$1 $TMP/$1
+    rm $PREFIX/share/fonts/$1/*Windows*
+    rm $TMP/$1.zip
+}
+
+CHECK=$(dialog --stdout \
+    --title "Nerd Fonts" --checklist "Select the desired fonts to download and install" 0 0 0 \
+    FiraMono "Mozilla typeface, dotted zero" off \
+    Meslo "Slashed zeros, customized version of Apple's Menlo" off \
+    Ubuntu "Specially created for Ubuntu" off \
+)
+
+if [ $? -eq 0 ]; then
+    if [[ ! $CHECK = *"FiraMono"* ]]; then rm -Rf $PREFIX/share/fonts/FiraMono; fi
+    if [[ ! $CHECK = *"Meslo"* ]]; then rm -Rf $PREFIX/share/fonts/Meslo; fi
+    if [[ ! $CHECK = *"Ubuntu"* ]]; then rm -Rf $PREFIX/share/fonts/Ubuntu; fi
+
+    for ITEM in $CHECK; do
+        install_nerdfont $ITEM
+    done
+
+    fc-cache -fv $PREFIX/share/fonts
+fi
