6452837d1b137bcb9d899fc0acdbd751b69f666e
Timóteo Ramos <timoteoramos@ymail.com>
2022-04-29 21:04:47 -0300

    Jaybird 4.0.6

--- generic/jdbc.sh
+++ generic/jdbc.sh
@@ -3,15 +3,15 @@
 SETUP_DIR=/usr/local/share/java
 TEMP_FOLDER=/tmp
 
-wget -O $TEMP_FOLDER/jaybird-4.0.4.java11.zip https://github.com/FirebirdSQL/jaybird/releases/download/v4.0.4/jaybird-4.0.4.java11.zip
+wget -O $TEMP_FOLDER/jaybird-4.0.6.java11.zip https://github.com/FirebirdSQL/jaybird/releases/download/v4.0.6/jaybird-4.0.6.java11.zip
 wget -O $TEMP_FOLDER/jconn3.jar.zip http://www.java2s.com/Code/JarDownload/jconn3/jconn3.jar.zip
 wget -O $TEMP_FOLDER/sqljdbc4-3.0.jar.zip http://www.java2s.com/Code/JarDownload/sqljdbc4/sqljdbc4-3.0.jar.zip
 
 mkdir -p $SETUP_DIR
-unzip -d $SETUP_DIR/ $TEMP_FOLDER/jaybird-4.0.4.java11.zip jaybird-full-4.0.4.java11.jar
+unzip -d $SETUP_DIR/ $TEMP_FOLDER/jaybird-4.0.6.java11.zip jaybird-full-4.0.6.java11.jar
 unzip -d $SETUP_DIR/ $TEMP_FOLDER/jconn3.jar.zip
 unzip -d $SETUP_DIR/ $TEMP_FOLDER/sqljdbc4-3.0.jar.zip
 
-rm $TEMP_FOLDER/jaybird-4.0.4.java11.zip
+rm $TEMP_FOLDER/jaybird-4.0.6.java11.zip
 rm $TEMP_FOLDER/jconn3.jar.zip
 rm $TEMP_FOLDER/sqljdbc4-3.0.jar.zip
